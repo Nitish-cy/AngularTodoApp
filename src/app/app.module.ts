782e398d210063c412316c4630f361ceeb4d0cc7
Nitish.mishra1 <Nitish.mishra1@RBLLNHO0424>
2022-11-28 16:29:25 +0530

    AllTodoCommits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { TodosComponent } from './cocomponents/todos/todos.component';
+import { TodoFormComponent } from './cocomponents/todo-form/todo-form.component';
+import { HeadComponent } from './layout/head/head.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoFormComponent,
+    HeadComponent
   ],
   imports: [
+
     BrowserModule
   ],
   providers: [],
